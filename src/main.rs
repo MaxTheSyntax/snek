9b488d8ab09d98f5a9955b04146c7637e4603fbe
MaxTheSyntax <maks@x-net.pl>
2024-05-03 01:55:28 +0200

    settings applied mid-game now apply mid-game, ai works now

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,6 @@ use crossterm::{
 use std::fs::*;
 use std::io;
 use std::io::prelude::*;
-use std::thread::sleep;
 use std::time::Duration;
 use std::{collections::LinkedList, time::Instant};
 
@@ -43,38 +42,6 @@ struct Snake {
 
 fn main() {
     std::thread::sleep(Duration::from_millis(100));
-
-    // Path to the options.json file
-    let file_path = "options.json";
-
-    // Open the file in read mode
-    let mut file = File::open(file_path).expect("Failed to open file");
-
-    // Read the contents of the file into a string
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)
-        .expect("Failed to read file");
-
-    // Deserialize json
-    let settings: Value = serde_json::from_str(&contents).expect("Invalid JSON");
-
-    // Extract settings
-    let speed = settings["speed"].as_u64().expect("1 not found / !integer");
-    let boost_speed = settings["boost_speed"]
-        .as_u64()
-        .expect("2 not found / !integer");
-
-    let board_width: i32 = settings["width"].as_i64().expect("3") as i32;
-    let board_height: i32 = settings["height"].as_i64().expect("4") as i32;
-
-    let ai = settings["ai"].as_bool().expect("5 not found / !type");
-    let god = settings["god"].as_bool().expect("6 not found / !type");
-
-    let boost_key_str = settings["boost_key"]
-        .as_str()
-        .expect("boost_key not found / !type");
-    let boost_key_char = boost_key_str.chars().next().unwrap();
-    let boost_key: KeyCode = KeyCode::Char(boost_key_char);
 
     // // Get user to choose mode
     // let mut choice = String::new();
@@ -114,11 +81,13 @@ fn main() {
     let mut score = 0;
     let mut game_over = false;
 
-    let duration_ms_default = speed;
-    let mut duration_ms = duration_ms_default;
+    // let duration_ms_default = speed;
+    // let mut duration_ms = duration_ms_default;
 
     let mut speed_debounce: bool = false;
 
+    let (mut file_path, mut speed, mut boost_speed, mut board_width, mut board_height, mut ai, mut god, mut boost_key) = get_settings("options.json");
+    // let (board_width, board_height) = (get_settings().3, get_settings().4);
     // Draw initial game state
     draw_game(
         &snake,
@@ -132,9 +101,17 @@ fn main() {
     clear();
 
     let now = Instant::now();
+    let mut settings_changed = false;
 
     // Game loop
     while !game_over {
+        if settings_changed {
+            (file_path, speed, boost_speed, board_width, board_height, ai, god, boost_key) = get_settings("options.json");
+            settings_changed = false;
+        }
+
+        let duration_ms_default = speed;
+        let mut duration_ms = speed;
         // Draw game board
         draw_game(
             &snake,
@@ -150,18 +127,15 @@ fn main() {
         let mut head = snake.body.front().unwrap().clone();
 
         if ai == true {
-            if head.0 > food.0 {
-                snake.direction = change_direction(Direction::Left, snake.direction);
-            } else if head.0 < food.0 {
-                snake.direction = change_direction(Direction::Right, snake.direction);
-            } else if head.1 > food.1 {
-                snake.direction = change_direction(Direction::Up, snake.direction);
-            } else if head.1 < food.1 {
-                snake.direction = change_direction(Direction::Down, snake.direction);
-            }
+            if head.0 > food.0 && move_possible(head, &snake.body, Direction::Left, god) { snake.direction = change_direction(Direction::Left, snake.direction); }
+                else if head.0 < food.0 && move_possible(head, &snake.body, Direction::Right, god) { snake.direction = change_direction(Direction::Right, snake.direction); }
+                else if head.1 > food.1 && move_possible(head, &snake.body, Direction::Up, god) { snake.direction = change_direction(Direction::Up, snake.direction); }
+                else if head.1 < food.1 && move_possible(head, &snake.body, Direction::Down, god) { snake.direction = change_direction(Direction::Down, snake.direction); }
+                else { snake.direction = move_somewhere(head, &snake.body); }
         }
 
         // Handle player input
+        #[allow(unused_assignments)] // Had to add this shit cuz game thinks variables won't be read even tho they will
         if poll(Duration::from_millis(duration_ms)).unwrap() {
             if let Event::Key(KeyEvent { code, .. }) = read().unwrap() {
                 match code {
@@ -177,11 +151,11 @@ fn main() {
                     KeyCode::Right => {
                         snake.direction = change_direction(Direction::Right, snake.direction)
                     }
-
                     KeyCode::Char('o') => {
-                        write_file(file_path);
+                        let _ = write_file(file_path);
+                        settings_changed = true;
+                        (file_path, speed, boost_speed, board_width, board_height, ai, god, boost_key) = get_settings("options.json");
                     }
-
                     _ => {
                         if code == boost_key {
                             if speed_debounce == false {
@@ -291,9 +265,9 @@ fn move_possible(
     head: (i32, i32),
     body: &LinkedList<(i32, i32)>,
     direction: Direction,
-    mode: &str,
+    is_god: bool,
 ) -> bool {
-    if mode == "safe ai" {
+    if is_god {
         return true;
     }
 
@@ -314,13 +288,13 @@ fn move_possible(
 }
 
 fn move_somewhere(head: (i32, i32), body: &LinkedList<(i32, i32)>) -> Direction {
-    if move_possible(head, body, Direction::Left, "ai") {
+    if move_possible(head, body, Direction::Left, false) {
         return Direction::Left;
     }
-    if move_possible(head, body, Direction::Right, "ai") {
+    if move_possible(head, body, Direction::Right, false) {
         return Direction::Right;
     }
-    if move_possible(head, body, Direction::Up, "ai") {
+    if move_possible(head, body, Direction::Up, false) {
         return Direction::Up;
     }
     return Direction::Down;
@@ -368,6 +342,7 @@ fn write_file(file: &str) -> std::io::Result<()> {
     io::stdin()
         .read_line(&mut speed)
         .expect("Failed to read line");
+    if speed.trim() == "" { speed = get_settings("options_default.json").1.to_string(); }
     let speed_num: u32 = speed.trim().parse().expect("Please enter a valid number");
 
     // save boost speed
@@ -378,22 +353,26 @@ fn write_file(file: &str) -> std::io::Result<()> {
     io::stdin()
         .read_line(&mut boost_speed)
         .expect("input failed");
+    if boost_speed.trim() == "" { boost_speed = get_settings("options_default.json").2.to_string(); }
     let boost_num: u32 = boost_speed.trim().parse().expect("invalid input");
 
     // save height and width
     println!("Input height, we recommend values between 10 and 100 // default: 20");
     let mut height = String::new();
     io::stdin().read_line(&mut height).expect("input failed");
+    if height.trim() == "" { height = get_settings("options_default.json").4.to_string(); }
     let height_num: i64 = height.trim().parse().expect("invalid input");
 
     println!("Input width, we recommend values between 20 and 150 // default: 40");
     let mut width = String::new();
     io::stdin().read_line(&mut width).expect("input failed");
+    if width.trim() == "" { width = get_settings("options_default.json").3.to_string(); }
     let width_num: i64 = width.trim().parse().expect("invalid input");
 
     println!("Use our sophisticated open sourced AI for the marvelous gameplay? true/false // default: false");
     let mut ai = String::new();
     io::stdin().read_line(&mut ai).expect("input failed");
+    if ai.trim() == "" { ai = get_settings("options_default.json").5.to_string(); }
     let ai_bool: bool = match ai.trim().parse() {
         Ok(result) => result,
         Err(_) => {
@@ -405,7 +384,8 @@ fn write_file(file: &str) -> std::io::Result<()> {
     println!("Ignore damage? true/false // default: false");
     let mut god = String::new();
     io::stdin().read_line(&mut god).expect("input failed");
-    let god_value: bool = match ai.trim().parse() {
+    if god.trim() == "" { god = get_settings("options_default.json").6.to_string(); }
+    let god_value: bool = match god.trim().parse() {
         Ok(result) => result,
         Err(_) => {
             println!("Please enter 'true' or 'false'");
@@ -440,7 +420,42 @@ fn write_file(file: &str) -> std::io::Result<()> {
     // serialize json and write to file
     file.write_all(serde_json::to_string_pretty(&json_msg).unwrap().as_bytes())?;
 
+    clear();
+
     Ok(())
+}
+
+fn get_settings(file_path: &str) -> (&str, u64, u64, i32, i32, bool, bool, KeyCode) {
+    // Open the file in read mode
+    let mut file = File::open(file_path).expect("Failed to open file");
+
+    // Read the contents of the file into a string
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .expect("Failed to read file");
+
+    // Deserialize json
+    let settings: Value = serde_json::from_str(&contents).expect("Invalid JSON");
+
+    // Extract settings
+    let speed = settings["speed"].as_u64().expect("1 not found / !integer");
+    let boost_speed = settings["boost_speed"]
+        .as_u64()
+        .expect("2 not found / !integer");
+
+    let board_width: i32 = settings["width"].as_i64().expect("3") as i32;
+    let board_height: i32 = settings["height"].as_i64().expect("4") as i32;
+
+    let ai = settings["ai"].as_bool().expect("5 not found / !type");
+    let god = settings["god"].as_bool().expect("6 not found / !type");
+
+    let boost_key_str = settings["boost_key"]
+        .as_str()
+        .expect("boost_key not found / !type");
+    let boost_key_char = boost_key_str.chars().next().unwrap();
+    let boost_key: KeyCode = KeyCode::Char(boost_key_char);
+
+    return (file_path, speed, boost_speed, board_width, board_height, ai, god, boost_key)
 }
 
 fn draw_game_over(score: u32) {
